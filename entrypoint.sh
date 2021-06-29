7a43a87813f2f4f936d3876db8957784a806be10
antman666 <945360554@qq.com>
2021-06-29 22:35:52 +0800

    update entrypoint.sh for mine

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-git clone "https://aur.archlinux.org/$1.git"
+export PACKAGER="antman666 <945360554@qq.com>"
+
+git clone "https://github.com/antman666/$1.git"
 cd "$1"
 makepkg -sf --noconfirm --skippgpcheck
