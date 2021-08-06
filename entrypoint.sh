6ce8654cfb1f74654805901f4d2dc23b724be4ba
zjuyk <ownbyzjuyk@gmail.com>
2021-08-06 16:52:00 +0800

    add yay support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-git clone "https://aur.archlinux.org/$1.git"
-cd "$1"
-makepkg -sf --noconfirm
+# if there is no dependencies in AUR
+# git clone "https://aur.archlinux.org/$1.git"
+# cd "$1"
+# makepkg -sf --noconfirm
+
+# creat a new user to run aur helper `yay` (which can not run under root)
+useradd build -m
+# passwordless sudo access
+echo "build ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# install yay
+git clone "https://aur.archlinux.org/yay.git" /tmp/yay
+cd /tmp/yay
+makepkg -sf --install --noconfirm
+cd -
+
+# build packages
+sudo -u build yay -Sw $1 --noconfirm
+pwd
