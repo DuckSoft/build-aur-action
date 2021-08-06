1596071b82ad0b2fc441efd2793ae586322cf143
zjuyk <ownbyzjuyk@gmail.com>
2021-08-06 17:56:43 +0800

    use martynas image instead

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,4 @@
-FROM archlinux:latest
-
-# WORKAROUND for glibc 2.33 and old Docker
-# See https://github.com/actions/virtual-environments/issues/2658
-# Thanks to https://github.com/lxqt/lxqt-panel/pull/1562
-RUN patched_glibc=glibc-linux4-2.33-5-x86_64.pkg.tar.zst && \
-    curl -LO "https://repo.archlinuxcn.org/x86_64/$patched_glibc" && \
-    bsdtar -C / -xvf "$patched_glibc"
+FROM martynas/archlinux:latest
 
 # enable multilib repo and hack makepkg to run under root user
 RUN sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf && pacman -Syu base-devel git --noconfirm --overwrite '*' && sed -i '/E_ROOT/d' /usr/bin/makepkg
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,54 @@
 #!/bin/bash
 
+# fail whole script if any command fails
+set -e
+
 # if there is no dependencies in AUR
 # git clone "https://aur.archlinux.org/$1.git"
 # cd "$1"
 # makepkg -sf --noconfirm
 
-# creat a new user to run aur helper `yay` (which can not run under root)
-useradd build -m
-# passwordless sudo access
-echo "build ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+pkgname=$1
 
-# install yay
-git clone "https://aur.archlinux.org/yay.git" /tmp/yay
-cd /tmp/yay
-makepkg -sf --install --noconfirm
-cd -
+# there should be a subdirectory with the same name of pkgname
+# to satisfy `aurpublish`
+pkgbuild_dir=$(readlink "$pkgname" -f)
 
-pacman -Syu --noconfirm
+if [[ ! -d $pkgbuild_dir ]]; then
+    echo "$pkgbuild_dir should be a directory."
+    exit 1
+fi
+
+if [[ ! -e $pkgbuild_dir/PKGBUILD ]]; then
+    echo "$pkgbuild_dir does not contain a PKGBUILD file."
+    exit 1
+fi
+
+if [[ ! -e $pkgbuild_dir/.SRCINFO ]]; then
+    echo "$pkgbuild_dir does not contain a .SRCINFO file."
+    exit 1
+fi
+
+# fix directory permissions
+sudo chown -R build "$pkgbuild_dir"
+sudo chown -R build /github/home
 
 # build packages
-sudo -u build yay -Sw $1 --noconfirm
-pwd
+
+install_deps() {
+    # install the package dependencies
+    grep -E 'depends' .SRCINFO | \
+        sed -e 's/.*depends = //' -e 's/:.*//' | \
+        xargs yay -S --noconfirm
+}
+
+cd $pkgbuild_dir
+
+## check PKGBUILD
+namcap PKGBUILD
+# install dependencies
+install_deps
+# just makepkg
+makepkg --syncdeps --noconfirm
+
+cd -
