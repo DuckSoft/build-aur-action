9caf4cde023b12b4b91653b597812e0ce3b03427
antman666 <945360554@qq.com>
2021-08-07 16:50:51 +0800

    random update somethings

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,4 @@ export PACKAGER="antman666 <945360554@qq.com>"
 
 git clone "https://github.com/antman666/$1.git"
 cd "$1"
-LIST=`ls -F`
-if [[ `echo $LIST | if grep -q "/$";then echo 1;fi` == 1 ]]; then
-		for DIR in $LIST; do
-				cd $DIR
-				makepkg -sf --noconfirm --skippgpcheck
-				mv *.zst ..
-		done
-else
-		makepkg -sf --noconfirm --skippgpcheck
-fi
+makepkg -sf --noconfirm --skippgpcheck
