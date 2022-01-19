9cd3f52592872211bb4767e52c5c6578d7e4928f
antman666 <945360554@qq.com>
2022-01-20 00:44:19 +0800

    add packager

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-export PACKAGER="AntMan666 <945360554@qq.com>"
+# export PACKAGER="AntMan666 <945360554@qq.com>"
 
 git clone "https://github.com/antman666/$1.git"
 cd "$1"
+echo 'PACKAGER="AntMan666 <945360554@qq.com>"' >> /etc/makepkg.conf
 updpkgsums
 makepkg -sf --noconfirm --skippgpcheck
